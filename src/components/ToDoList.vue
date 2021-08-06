3a8fd26b41ea55ec1bf09dcd1bd8bafec633401b
Joel Silva <joel.a.silva1986@gmail.com>
2021-08-06 17:43:56 -0300

    first commit

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1> To-Do List</h1>
+        <input v-model.trim="task"
+            type="text"
+            placeholder="Adicionar novo To-Do"
+            >
+        <button @click="addTask" type="button">Adicionar</button>
+        <ul>
+            <li v-for="task in tasks" :key="task">{{ task }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ToDoList",
+    data() {
+        return {
+            task: "",
+            tasks: []
+        }
+    },
+    methods: {
+        addTask() {
+            this.tasks.push(this.task)
+            this.task=""
+        }
+    }
+}
+</script>
